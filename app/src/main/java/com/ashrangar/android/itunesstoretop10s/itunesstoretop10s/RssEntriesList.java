9ab87759fbee70dd6174da58776be8a4c3a64a79
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 17:33:05 -0600

    initialize RssEntries

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssEntriesList.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssEntriesList.java
@@ -12,4 +12,17 @@ public class RssEntriesList {
         mRssEntries = new ArrayList<RssEntry>();
         initializeRssEntries();
     }
+
+    private void initializeRssEntries() {
+        mRssEntries.add(new RssEntry("Songs", ""));
+        mRssEntries.add(new RssEntry("Albums", ""));
+        mRssEntries.add(new RssEntry("New Releases", ""));
+        mRssEntries.add(new RssEntry("Just Added", ""));
+        mRssEntries.add(new RssEntry("Featured Albums & Exclusives", ""));
+        mRssEntries.add(new RssEntry("Paid Apps", ""));
+        mRssEntries.add(new RssEntry("Free Apps", ""));
+        mRssEntries.add(new RssEntry("Movies", ""));
+        mRssEntries.add(new RssEntry("TV Seasons", ""));
+        mRssEntries.add(new RssEntry("TV Episodes", ""));
+    }
 }
