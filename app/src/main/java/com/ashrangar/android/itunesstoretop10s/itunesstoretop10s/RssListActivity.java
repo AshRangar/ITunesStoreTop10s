36cb0ae790a11a0d1f9beb7c73ff7e1953748acc
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 16:31:57 -0600

    adds rssListView

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssListActivity.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssListActivity.java
@@ -1,13 +1,20 @@
 package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class RssListActivity extends AppCompatActivity {
+
+    private ListView mRssListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rss_list);
+
+        mRssListView = (ListView) findViewById(R.id.rssListView);
+        
     }
+
 }
