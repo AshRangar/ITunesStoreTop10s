f26a7d7de3760ca4a501540b99539a21df65e32f
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 17:31:51 -0600

    adds RssEntriesList class

---
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssEntriesList.java
@@ -0,0 +1,9 @@
+package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ashwin on 1/25/16.
+ */
+public class RssEntriesList {
+}
--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssEntry.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssEntry.java
@@ -4,9 +4,11 @@ package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
  * Created by ashwin on 1/25/16.
  */
 public class RssEntry {
+    private String title;
     private String url;
 
-    public RssEntry(String url) {
+    public RssEntry(String title, String url) {
+        this.title = title;
         this.url = url;
     }
 
@@ -16,5 +18,17 @@ public class RssEntry {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String toString() {
+        return "Top 10 " + title;
     }
 }
