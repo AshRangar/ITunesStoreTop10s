50ab6ff9c5f8ca6a8b9c2fef7a6bbde6a0c4db1d
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 16:44:46 -0600

    adds RssEntry Class

---
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssEntry.java
@@ -0,0 +1,20 @@
+package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
+
+/**
+ * Created by ashwin on 1/25/16.
+ */
+public class RssEntry {
+    private String url;
+
+    public RssEntry(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
