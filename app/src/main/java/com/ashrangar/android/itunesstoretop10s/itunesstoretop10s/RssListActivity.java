9dd79f8a97170ba48da7ff417de71dd32c5ca95a
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 17:33:58 -0600

    adds arrayAdapter for mRssEntriesList

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssListActivity.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssListActivity.java
@@ -2,11 +2,16 @@ package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class RssListActivity extends AppCompatActivity {
 
     private ListView mRssListView;
+    private RssEntriesList mRssEntriesList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +19,13 @@ public class RssListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rss_list);
 
         mRssListView = (ListView) findViewById(R.id.rssListView);
-        
+        mRssEntriesList = new RssEntriesList();
+
+        ArrayAdapter<RssEntry> arrayAdapter = new ArrayAdapter<RssEntry>(
+                RssListActivity.this, R.layout.list_view, mRssEntriesList.getRssEntries()
+        );
+
+        mRssListView.setAdapter(arrayAdapter);
     }
 
 }
