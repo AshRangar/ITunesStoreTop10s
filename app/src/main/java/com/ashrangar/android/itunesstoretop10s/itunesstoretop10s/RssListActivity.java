05dd440d7964916c5fce0b9f5733c88eea9ce658
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 16:11:36 -0600

    Initial commit

---
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssListActivity.java
@@ -0,0 +1,13 @@
+package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RssListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rss_list);
+    }
+}
