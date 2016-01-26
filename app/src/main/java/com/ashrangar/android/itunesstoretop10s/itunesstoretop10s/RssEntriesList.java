14647fe11134762b13072655c8959a18650777c4
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 23:30:08 -0600

    adds the url path

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssEntriesList.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssEntriesList.java
@@ -14,16 +14,16 @@ public class RssEntriesList {
     }
 
     private void initializeRssEntries() {
-        mRssEntries.add(new RssEntry("Songs", ""));
-        mRssEntries.add(new RssEntry("Albums", ""));
-        mRssEntries.add(new RssEntry("New Releases", ""));
-        mRssEntries.add(new RssEntry("Just Added", ""));
-        mRssEntries.add(new RssEntry("Featured Albums & Exclusives", ""));
-        mRssEntries.add(new RssEntry("Paid Apps", ""));
-        mRssEntries.add(new RssEntry("Free Apps", ""));
-        mRssEntries.add(new RssEntry("Movies", ""));
-        mRssEntries.add(new RssEntry("TV Seasons", ""));
-        mRssEntries.add(new RssEntry("TV Episodes", ""));
+        mRssEntries.add(new RssEntry("Songs", "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml"));
+        mRssEntries.add(new RssEntry("Albums", "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topalbums/limit=10/xml"));
+        mRssEntries.add(new RssEntry("New Releases", "http://ax.itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/limit=10/rss.xml"));
+        mRssEntries.add(new RssEntry("Just Added", "http://ax.itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/justadded/limit=10/rss.xml"));
+        mRssEntries.add(new RssEntry("Featured Albums", "http://ax.itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/featuredalbums/limit=10/rss.xml"));
+        mRssEntries.add(new RssEntry("Paid Apps", "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=10/xml"));
+        mRssEntries.add(new RssEntry("Free Apps", "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml"));
+        mRssEntries.add(new RssEntry("Movies", "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/xml"));
+        mRssEntries.add(new RssEntry("TV Seasons", "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topTvSeasons/xml"));
+        mRssEntries.add(new RssEntry("TV Episodes", "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topTvEpisodes/xml"));
     }
 
     public ArrayList<RssEntry> getRssEntries() {
