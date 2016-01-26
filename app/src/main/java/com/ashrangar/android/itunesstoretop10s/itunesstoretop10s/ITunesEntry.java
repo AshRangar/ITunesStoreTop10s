3ddd6caba0ab79a8de45d3ba21e77ef4c9876c1d
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 23:35:47 -0600

    adds members and constructors

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/ITunesEntry.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/ITunesEntry.java
@@ -4,4 +4,19 @@ package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
  * Created by ashwin on 1/25/16.
  */
 public class ITunesEntry {
+    private String name;
+    private String artist;
+    private String releaseDate;
+
+    public ITunesEntry() {
+        this.name = new String();
+        this.artist = new String();
+        this.releaseDate = new String();
+    }
+
+    public ITunesEntry(String name, String artist, String releaseDate) {
+        this.name = name;
+        this.artist = artist;
+        this.releaseDate = releaseDate;
+    }
 }
