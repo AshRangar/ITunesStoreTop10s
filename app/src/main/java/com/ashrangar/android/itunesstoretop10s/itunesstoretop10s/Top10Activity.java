28e8173f9bf16661dfa68971ffd2f7d1bc11ac95
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 23:36:52 -0600

    adds onCreate method

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/Top10Activity.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/Top10Activity.java
@@ -17,9 +17,28 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class Top10Activity extends AppCompatActivity {
+
     private final String TAG = Top10Activity.class.getSimpleName().toString();
     private RssEntry mRssEntry;
     private ListView mEntriesListView;
     private Toolbar mToolbar;
     private String mXmlData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rss_list);
+
+        mEntriesListView = (ListView) findViewById(R.id.rssListView);
+
+        Intent intent = getIntent();
+        mRssEntry = (RssEntry) intent.getSerializableExtra(getString(R.string.rss_entry_key));
+
+        XmlData xmlData = new XmlData();
+        xmlData.execute(mRssEntry.getUrl());
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(mRssEntry.getTitle());
+
+    }
 }
