a40555610327a1bffd50c9f180dafcb1565e43b2
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 23:35:57 -0600

    adds getters and setters

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/ITunesEntry.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/ITunesEntry.java
@@ -19,4 +19,32 @@ public class ITunesEntry {
         this.artist = artist;
         this.releaseDate = releaseDate;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void populateData() {
+
+    }
 }
