57d216737fc95ef5ed09c6e99144de7def826bb4
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 23:34:23 -0600

    adds ITunesEntriesFactory class

---
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/ITunesEntriesFactory.java
@@ -0,0 +1,15 @@
+package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
+
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+
+/**
+ * Created by ashwin on 1/25/16.
+ */
+public class ITunesEntriesFactory {
+}
