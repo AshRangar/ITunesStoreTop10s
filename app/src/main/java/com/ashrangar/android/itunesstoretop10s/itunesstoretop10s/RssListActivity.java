de5f2cf78e9a21278e281f089090bb5c7c8dc45a
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 23:27:21 -0600

    update import list

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssListActivity.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssListActivity.java
@@ -1,11 +1,14 @@
 package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class RssListActivity extends AppCompatActivity {
 
