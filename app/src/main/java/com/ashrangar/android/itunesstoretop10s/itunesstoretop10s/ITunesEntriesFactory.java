10e173723fcf6095c22535096599935c945fcc34
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 23:34:49 -0600

    adds the getItunesEntries method

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/ITunesEntriesFactory.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/ITunesEntriesFactory.java
@@ -12,4 +12,16 @@ import java.util.ArrayList;
  * Created by ashwin on 1/25/16.
  */
 public class ITunesEntriesFactory {
+    private String mXmlData;
+    private ArrayList<ITunesEntry> mITunesEntries;
+
+    private final String TAG = ITunesEntriesFactory.class.getSimpleName().toString();
+
+
+    public ArrayList<ITunesEntry> getITunesEntries(String xmlData) {
+        mITunesEntries = new ArrayList<ITunesEntry>();
+        mXmlData = xmlData;
+        process();
+        return mITunesEntries;
+    }
 }
