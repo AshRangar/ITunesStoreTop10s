1b7656ee0862f2e3496e78726a1faa21df951cda
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 23:30:31 -0600

    implements Serializable to pass from one activity to another

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssEntriesList.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/RssEntriesList.java
@@ -1,11 +1,12 @@
 package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by ashwin on 1/25/16.
  */
-public class RssEntriesList {
+public class RssEntriesList implements Serializable{
     private ArrayList<RssEntry> mRssEntries;
 
     public RssEntriesList() {
