5130c14327a84b43f0043c79a36d993e235043a1
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-25 23:36:29 -0600

    creates Top10Activity

---
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/Top10Activity.java
@@ -0,0 +1,20 @@
+package com.ashrangar.android.itunesstoretop10s.itunesstoretop10s;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Top10Activity extends AppCompatActivity {
+}
