c7e7a53ecd507842fcc120234e717242f0011eef
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-04-11 19:08:42 -0500

    adds fix

--- app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/ITunesEntriesFactory.java
+++ app/src/main/java/com/ashrangar/android/itunesstoretop10s/itunesstoretop10s/ITunesEntriesFactory.java
@@ -10,14 +10,18 @@ import java.util.ArrayList;
 
 /**
  * Created by ashwin on 1/25/16.
+ *
+ * Based on the category, the XML data would be parsed and the top ten ITunesEntry objects would be obtained.
+ *
  */
 public class ITunesEntriesFactory {
+
     private String mXmlData;
     private ArrayList<ITunesEntry> mITunesEntries;
-
     private final String TAG = ITunesEntriesFactory.class.getSimpleName().toString();
 
 
+    // Constructor
     public ArrayList<ITunesEntry> getITunesEntries(String xmlData) {
         mITunesEntries = new ArrayList<ITunesEntry>();
         mXmlData = xmlData;
@@ -26,7 +30,6 @@ public class ITunesEntriesFactory {
     }
 
     public boolean process() {
-        boolean status = true;
         ITunesEntry currentRecord = null;
         boolean inEntry = false;
         String textValue = "";
@@ -43,7 +46,7 @@ public class ITunesEntriesFactory {
                 String tagName = xpp.getName();
                 switch (eventType) {
                     case XmlPullParser.START_TAG:
-                        Log.d(TAG, "Starting tag for " + tagName);
+                        //Log.d(TAG, "Starting tag for " + tagName);
 
                         if (tagName.equalsIgnoreCase("entry") || tagName.equalsIgnoreCase("item")) {
                             inEntry = true;
@@ -57,20 +60,22 @@ public class ITunesEntriesFactory {
                         break;
 
                     case XmlPullParser.END_TAG:
-                        Log.d(TAG, "Ending tag for " + tagName);
+                        //Log.d(TAG, "Ending tag for " + tagName);
 
                         if (inEntry) {
                             if (tagName.equalsIgnoreCase("entry") || tagName.equalsIgnoreCase("item")) {
-                                Log.d(TAG, "HEREEEEE! in item");
                                 mITunesEntries.add(currentRecord);
                                 inEntry = false;
                             } else if (tagName.equalsIgnoreCase("name") || tagName.equalsIgnoreCase("title")) {
-                                Log.d(TAG, "HEREEEEE! in title");
                                 currentRecord.setName(textValue);
                             } else if (tagName.equalsIgnoreCase("artist")) {
                                 currentRecord.setArtist(textValue);
                             } else if (tagName.equalsIgnoreCase("releaseDate")) {
                                 currentRecord.setReleaseDate(textValue);
+                            } else if (tagName.equalsIgnoreCase("id")) {
+                                currentRecord.setUrl(textValue);
+                            } else if (tagName.equalsIgnoreCase("image") || tagName.equalsIgnoreCase("coverart")) {
+                                currentRecord.setImageUrl(textValue);
                             }
                         }
 
@@ -80,7 +85,6 @@ public class ITunesEntriesFactory {
                 eventType = xpp.next();
             }
         } catch (Exception e) {
-            status = false;
             e.printStackTrace();
         }
 
